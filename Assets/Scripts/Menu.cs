144b40c04f496e153d776e3ae995f52bf188081f
lovelasi4ek <lord1of2ultima@gmail.com>
2019-09-26 22:58:03 +0300

    Электричество рабочий вариант

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Menu : MonoBehaviour
+{
+    public GameObject gg;
+    public GameObject gg1;
+    public GameObject gg2;
+    public GameObject gg3;
+    public Text text;
+    // Start is called before the first frame update
+    
+  public void Start1()
+    {
+       
+
+        if ((gg.activeSelf == true)) //&& (gg1.activeSelf == true))
+        {
+            
+            gg2.transform.position = gg1.transform.position;
+            gg1.transform.position = gg.transform.position;
+            gg.SetActive(false);
+            gg2.SetActive(true);
+
+        }
+        if ((gg1.activeSelf == true) && (gg2.activeSelf == true))
+        {
+            gg3.transform.position = gg2.transform.position;
+            gg2.transform.position = gg1.transform.position;
+            gg1.SetActive(false);
+            gg3.SetActive(true);
+
+        }
+        if ((gg3.activeSelf == true) && (gg2.activeSelf == true))
+        {
+            gg.transform.position = gg3.transform.position;
+            gg3.transform.position = gg2.transform.position;
+            gg2.SetActive(false);
+            gg.SetActive(true);
+
+        }
+        if ((gg3.activeSelf == true) && (gg.activeSelf == true))
+        {
+            gg1.transform.position = gg.transform.position;
+            gg.transform.position = gg3.transform.position;
+            gg3.SetActive(false);
+            gg1.SetActive(true);
+
+        }
+    }
+
+    // Update is called once per frame
+  
+}
--- Assets/Scripts/Recolocation1.cs
+++ Assets/Scripts/Recolocation1.cs
@@ -12,26 +12,29 @@ public class Recolocation1 : MonoBehaviour
 {
     public SerialPort sp = new SerialPort("COM3", 38400);
     public bool B_Check;
-    public GameObject lamp1;
-   /* public GameObject lamp2;
-    public GameObject lamp3;
-    public GameObject lamp4;
-    public GameObject lamp5;
-    public GameObject lamp6;
-    public GameObject lamp7;
-    public GameObject lamp8;
-    public GameObject lamp9;
-    public GameObject lamp10;
-    public GameObject lamp11;
-    public GameObject lamp12;
-    public GameObject lamp13;
-    public GameObject lamp14;
-    public GameObject lamp15;
-    public GameObject lamp16;
-    public GameObject lamp17;
-    public GameObject lamp18;
-    public GameObject lamp19;
-    public GameObject lamp20;*/
+    public GameObject[] lamp1 = new GameObject[21];
+    public GameObject[] gg = new GameObject[5];
+
+
+    /* public GameObject lamp2;
+     public GameObject lamp3;
+     public GameObject lamp4;
+     public GameObject lamp5;
+     public GameObject lamp6;
+     public GameObject lamp7;
+     public GameObject lamp8;
+     public GameObject lamp9;
+     public GameObject lamp10;
+     public GameObject lamp11;
+     public GameObject lamp12;
+     public GameObject lamp13;
+     public GameObject lamp14;
+     public GameObject lamp15;
+     public GameObject lamp16;
+     public GameObject lamp17;
+     public GameObject lamp18;
+     public GameObject lamp19;
+     public GameObject lamp20;*/
     public Text text1;
     public void Start1()
     {
@@ -40,7 +43,7 @@ public class Recolocation1 : MonoBehaviour
             B_Check = true;
             sp.Open();
             text1.text = "";
-         
+
         }
         else
         {
@@ -48,6 +51,9 @@ public class Recolocation1 : MonoBehaviour
             sp.Close();
             text1.text = "";
         }
+        //lamp1 = GameObject.Find("New Sprite (2)");
+
+
         InvokeRepeating("Resolocation", 0.1f, 0.1f);
         // Process.Start("C:\\Users/lord1/source/repos/ConsoleApp15/ConsoleApp15/bin/Debug/ConsoleApp15.exe");
         // Invoke("Resolocation", 0.1f);
@@ -60,24 +66,50 @@ public class Recolocation1 : MonoBehaviour
             string s = sp.ReadLine();
             s = s.Substring(s.IndexOf('L') + 1, s.IndexOf('T') - s.IndexOf('L') - 1);
             int f = (Convert.ToInt32(s));
-    
-            text1.text = f.ToString();
-            int n = f / 30;
-            if (n<=5)
-            {
-                for (int i = 1; i < n; i++)
-                {
-                    lamp1 = GameObject.Find("shkala2 (" + i.ToString()+ ")");
-                    lamp1.SetActive(false);
-                }
-                if (n < 5)
-                    for (int i = n+1; i < 5; i++)
-                    {
-                        lamp1 = GameObject.Find("shkala2 (" + i.ToString() + ")");
-                        lamp1.SetActive(true);
-                    }
-            }
 
+            text1.text = f.ToString();
+            int n = f / 60;
+            if (n >= 1)
+            {
+                if (n <= 21)
+                {
+                    for (int i = 1; i < n; i++)
+                    {
+
+                        lamp1[i].SetActive(true);
+                    }
+                    if (n < 21)
+                        for (int i = n; i < 21; i++)
+                        {
+
+                            lamp1[i].SetActive(false);
+                        }
+                }
+
+            }
+        }
+    }
+    void Update()
+    {
+        for (int i = 1; i < 21; i ++)
+        {
+            if (lamp1[i].activeSelf == true)
+            {
+                int j = i / 5;
+                for (int k = 0; k < j; k++)
+                    gg[k].SetActive(true);
+            }
+            if (i < 21)
+            {
+                int a = i / 5;
+                for (int e = a; e < 4; e++)
+                    gg[e].SetActive(false);
+            }
         }
     }
 }
+
+
+    
+
+
