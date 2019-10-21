ce8239cc3deffdf31beb023bd337ddee661ce09e
wmadwand <wmadwand@gmail.com>
2019-10-21 15:22:20 +0300

    Import another one class for sound processing

---
+++ Assets/Scripts/SoundReader02.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundReader02 : MonoBehaviour
+{
+	public float sensitivity = 100;
+	public float loudness = 0;
+	private AudioSource _audio;
+
+	void Awake()
+	{
+		_audio = GetComponent<AudioSource>();
+	}
+
+	void Start()
+	{
+		_audio.clip = Microphone.Start(null, true, 10, 44100);
+		_audio.loop = true;
+		_audio.mute = false;
+		while (!(Microphone.GetPosition(null) > 0)) { }
+		_audio.Play();
+	}
+
+	void Update()
+	{
+		loudness = GetAveragedVolume() * sensitivity;
+		if (loudness > 1)
+		{
+			Debug.Log($"loudness: {loudness}");
+		}
+	}
+
+	float GetAveragedVolume()
+	{
+		float[] data = new float[256];
+		float a = 0;
+		_audio.GetOutputData(data, 0);
+		foreach (float s in data)
+		{
+			a += Mathf.Abs(s);
+		}
+		return a / 256;
+	}
+}
