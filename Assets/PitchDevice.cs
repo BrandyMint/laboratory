0919c4ce461c743bde79b8a724f6dc6f0c6b80d5
wmadwand <wmadwand@gmail.com>
2019-10-22 16:13:49 +0300

    EventsManager for debugging purposes

--- Assets/PitchDevice.cs
+++ Assets/PitchDevice.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class PitchDevice : MonoBehaviour
 {
     public TextMeshProUGUI text;
+    public RectTransform panel;
+
+    private void Awake()
+    {
+        SoundReader02.OnAnalyzeSound += SoundReader02_OnAnalyzeSound;
+        EventsManager.OnPitchDeviceSetActive += EventsManager_OnPitchDeviceSetActive;
+    }
 
     private void Start()
     {
-        SoundReader02.OnAnalyzeSound += SoundReader02_OnAnalyzeSound;
+
+
+        //panel.DOAnchorPosY(900, 0);
+    }
+
+    private void EventsManager_OnPitchDeviceSetActive(bool obj)
+    {
+        SetActive(obj);
     }
 
     private void SoundReader02_OnAnalyzeSound(float[] obj)
@@ -20,5 +35,19 @@ public class PitchDevice : MonoBehaviour
     private void OnDestroy()
     {
         SoundReader02.OnAnalyzeSound -= SoundReader02_OnAnalyzeSound;
+    }
+
+    public void SetActive(bool value)
+    {
+        if (value)
+        {
+            panel.DOAnchorPosY(2.5f, 1);
+        }
+        else
+        {
+            panel.DOAnchorPosY(900, 0);
+        }
+        
+        
     }
 }
---
+++ Assets/_Project/Scripts/EventsManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsManager : MonoBehaviour
+{
+    public static event Action<bool> OnPitchDeviceSetActive;
+    public static event Action<bool> OnVolumeDeviceSetActive;
+
+    bool b1, b2;
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        PitchDeviceSetActive();
+        VolumeDeviceSetActive();
+    }
+
+    public void PitchDeviceSetActive()
+    {
+        OnPitchDeviceSetActive?.Invoke(b1);
+        b1 = !b1;
+    }
+
+    public void VolumeDeviceSetActive()
+    {
+        OnVolumeDeviceSetActive?.Invoke(b2);
+        b2 = !b2;
+    }
+}
