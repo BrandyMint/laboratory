1fbf650a9352175b1855e11221692f06c24a3193
wmadwand <wmadwand@gmail.com>
2019-10-22 14:30:25 +0300

    Show pitch value through UI text

---
+++ Assets/PitchDevice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PitchDevice : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    private void Start()
+    {
+        SoundReader02.OnAnalyzeSound += SoundReader02_OnAnalyzeSound;
+    }
+
+    private void SoundReader02_OnAnalyzeSound(string[] obj)
+    {
+        text.text = $"{obj[1]} Hz";
+    }
+
+    private void OnDestroy()
+    {
+        SoundReader02.OnAnalyzeSound -= SoundReader02_OnAnalyzeSound;
+    }
+}
--- Assets/_Project/Scripts/SoundReader02.cs
+++ Assets/_Project/Scripts/SoundReader02.cs
@@ -1,114 +1,123 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SoundReader02 : MonoBehaviour
 {
-	public Text dbValText;
-	public Text pitchValText;
-	public PointerBehaviour pointerBehaviour;
+    public static event Action<string[]> OnAnalyzeSound;
 
-	public float sensitivity = 100;
-	public float loudness = 0;
-	private AudioSource _audio;
+    public Text dbValText;
+    public Text pitchValText;
+    public PointerBehaviour pointerBehaviour;
 
-	public float rmsVal;
-	public float dbVal;
-	public float pitchVal;
+    public float sensitivity = 100;
+    public float loudness = 0;
+    private AudioSource _audio;
 
-	private const int QSamples = 1024;
-	private const float RefValue = 0.1f;
-	private const float Threshold = 0.02f;
+    public float rmsVal;
+    public float dbVal;
+    public float pitchVal;
 
-	float[] _samples;
-	private float[] _spectrum;
-	private float _fSample;
+    private const int QSamples = 1024;
+    private const float RefValue = 0.1f;
+    private const float Threshold = 0.02f;
 
-	void Awake()
-	{
-		_audio = GetComponent<AudioSource>();
-	}
+    float[] _samples;
+    private float[] _spectrum;
+    private float _fSample;
 
-	void Start()
-	{
-		_audio.clip = Microphone.Start(null, true, 10, 44100);
-		_audio.loop = true;
-		_audio.mute = false;
-		while (!(Microphone.GetPosition(null) > 0)) { }
-		_audio.Play();
+    string[] vals = new string[2];
 
-		_samples = new float[QSamples];
-		_spectrum = new float[QSamples];
-		_fSample = AudioSettings.outputSampleRate;
-	}
+    void Awake()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
 
-	void Update()
-	{
-		//loudness = GetAveragedVolume() * sensitivity;
-		//if (loudness > 1)
-		//{
-		//	Debug.Log($"loudness: {loudness}");
-		//}
+    void Start()
+    {
+        _audio.clip = Microphone.Start(null, true, 10, 44100);
+        _audio.loop = true;
+        _audio.mute = false;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        _audio.Play();
 
-		AnalyzeSound();
+        _samples = new float[QSamples];
+        _spectrum = new float[QSamples];
+        _fSample = AudioSettings.outputSampleRate;
+    }
 
-		Debug.Log("RMS: " + rmsVal.ToString("F2"));
-		Debug.Log(dbVal.ToString("F1") + " dB");
-		Debug.Log(pitchVal.ToString("F0") + " Hz");
+    void Update()
+    {
+        //loudness = GetAveragedVolume() * sensitivity;
+        //if (loudness > 1)
+        //{
+        //	Debug.Log($"loudness: {loudness}");
+        //}
 
-		dbValText.text = $"Volume: {dbVal.ToString("F1")} dB";
-		pitchValText.text = $"Frequency: {pitchVal.ToString("F0")} Hz";
+        AnalyzeSound();
 
-		//dbValText.text = $"VolumeAS: {_audio.volume} dB";
-		//pitchValText.text = $"FrequencyAS: {_audio.pitch} Hz";
+        vals = new string[] { dbVal.ToString("F1"), pitchVal.ToString("F0") };
 
-		pointerBehaviour.Rotate(dbVal * -1);
-	}
+        OnAnalyzeSound?.Invoke(vals);
 
-	float GetAveragedVolume()
-	{
-		float[] data = new float[256];
-		float a = 0;
-		_audio.GetOutputData(data, 0);
-		foreach (float s in data)
-		{
-			a += Mathf.Abs(s);
-		}
-		return a / 256;
-	}
+        Debug.Log("RMS: " + rmsVal.ToString("F2"));
+        Debug.Log(dbVal.ToString("F1") + " dB");
+        Debug.Log(pitchVal.ToString("F0") + " Hz");
 
-	void AnalyzeSound()
-	{
-		_audio.GetOutputData(_samples, 0); // fill array with samples
-		int i;
-		float sum = 0;
-		for (i = 0; i < QSamples; i++)
-		{
-			sum += _samples[i] * _samples[i]; // sum squared samples
-		}
-		rmsVal = Mathf.Sqrt(sum / QSamples); // rms = square root of average
-		dbVal = 20 * Mathf.Log10(rmsVal / RefValue); // calculate dB
-		if (dbVal < -160) dbVal = -160; // clamp it to -160dB min
-										// get sound spectrum
-		_audio.GetSpectrumData(_spectrum, 0, FFTWindow.BlackmanHarris);
-		float maxV = 0;
-		var maxN = 0;
-		for (i = 0; i < QSamples; i++)
-		{ // find max 
-			if (!(_spectrum[i] > maxV) || !(_spectrum[i] > Threshold))
-				continue;
+        dbValText.text = $"Volume: {dbVal.ToString("F1")} dB";
+        pitchValText.text = $"Frequency: {pitchVal.ToString("F0")} Hz";
 
-			maxV = _spectrum[i];
-			maxN = i; // maxN is the index of max
-		}
-		float freqN = maxN; // pass the index to a float variable
-		if (maxN > 0 && maxN < QSamples - 1)
-		{ // interpolate index using neighbours
-			var dL = _spectrum[maxN - 1] / _spectrum[maxN];
-			var dR = _spectrum[maxN + 1] / _spectrum[maxN];
-			freqN += 0.5f * (dR * dR - dL * dL);
-		}
-		pitchVal = freqN * (_fSample / 2) / QSamples; // convert index to frequency
-	}
+        //dbValText.text = $"VolumeAS: {_audio.volume} dB";
+        //pitchValText.text = $"FrequencyAS: {_audio.pitch} Hz";
+
+        pointerBehaviour.Rotate(dbVal * -1);
+    }
+
+    float GetAveragedVolume()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        _audio.GetOutputData(data, 0);
+        foreach (float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+        return a / 256;
+    }
+
+    void AnalyzeSound()
+    {
+        _audio.GetOutputData(_samples, 0); // fill array with samples
+        int i;
+        float sum = 0;
+        for (i = 0; i < QSamples; i++)
+        {
+            sum += _samples[i] * _samples[i]; // sum squared samples
+        }
+        rmsVal = Mathf.Sqrt(sum / QSamples); // rms = square root of average
+        dbVal = 20 * Mathf.Log10(rmsVal / RefValue); // calculate dB
+        if (dbVal < -160) dbVal = -160; // clamp it to -160dB min
+                                        // get sound spectrum
+        _audio.GetSpectrumData(_spectrum, 0, FFTWindow.BlackmanHarris);
+        float maxV = 0;
+        var maxN = 0;
+        for (i = 0; i < QSamples; i++)
+        { // find max 
+            if (!(_spectrum[i] > maxV) || !(_spectrum[i] > Threshold))
+                continue;
+
+            maxV = _spectrum[i];
+            maxN = i; // maxN is the index of max
+        }
+        float freqN = maxN; // pass the index to a float variable
+        if (maxN > 0 && maxN < QSamples - 1)
+        { // interpolate index using neighbours
+            var dL = _spectrum[maxN - 1] / _spectrum[maxN];
+            var dR = _spectrum[maxN + 1] / _spectrum[maxN];
+            freqN += 0.5f * (dR * dR - dL * dL);
+        }
+        pitchVal = freqN * (_fSample / 2) / QSamples; // convert index to frequency
+    }
 }
