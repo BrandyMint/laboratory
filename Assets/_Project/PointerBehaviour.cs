1224c2c2f990d2c8505e4d9228e2a5ea1b945471
wmadwand <wmadwand@gmail.com>
2019-10-22 10:50:06 +0300

    Small fix for Pointer

--- Assets/_Project/PointerBehaviour.cs
+++ Assets/_Project/PointerBehaviour.cs
@@ -7,6 +7,6 @@ public class PointerBehaviour : MonoBehaviour
    public void Rotate(float val)
 	{
 		var angle = Mathf.Lerp(500, 300, Mathf.InverseLerp(0, 100, val));
-		transform.eulerAngles = new Vector3(0, 0, angle);
+		transform.eulerAngles = new Vector3(0, 0, -angle);
 	}
 }
