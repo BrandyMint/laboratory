36c242fbc6b9fd1670bf48adfc3113bdf26d4a9f
wmadwand <wmadwand@gmail.com>
2019-10-23 15:45:30 +0300

    Basic Sound system

---
+++ Assets/_Project/Scripts/SoundSystem/Sound.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public enum Sounds
+{
+    bubbleClick,
+    bubbleExplosion,
+    feedFishGood,
+	feedFishBad,
+    fishDead,
+	fishHappy,
+	fishSpawn,
+	backgroundMusic,
+	fishSwap,
+	tubeProduceBubble,
+	bubbleSwipe,
+	evilFishCome
+}
+
+[Serializable]
+public struct Sound
+{
+    public Sounds name;
+    public AudioClip audioClip;
+    public float volume;
+}
